043470c04bee2e40cad6cc28e6d2b9c2c4add2b2
NicSTZ <nicvdvideo@gmail.com>
2026-09-14 15:43:16 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -69,7 +69,7 @@ static String osdMediaText() {
 void setup() {
     Serial.begin(115200);
     delay(250);
-    diagLog("BOOT: FPVCineCam32 v0.10.2 media capture");
+    diagLog("BOOT: FPVCineCam32 v0.10.3 packet capture");
     Serial.printf("[%8lu ms] resetReason=%d freeHeap=%u\n", (unsigned long)millis(), (int)esp_reset_reason(), (unsigned)ESP.getFreeHeap());
     diagLog("SETTINGS: begin");
     settingsStore.begin();
